201f2b2a01d804260b9839875864659b1771b156
Chulhee Lee <chulhee23@naver.com>
2022-02-26 16:49:48 +0900

    spring cloud gateway - header check

--- second-service/src/main/kotlin/im/chulhee/secondservice/controller/SecondServiceController.kt
+++ second-service/src/main/kotlin/im/chulhee/secondservice/controller/SecondServiceController.kt
@@ -1,15 +1,21 @@
 package im.chulhee.secondservice.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/second-service")
 class SecondServiceController {
+    private val logger = LoggerFactory.getLogger(SecondServiceController::class.java)
 
     @GetMapping("/welcome")
-    fun welcome() : String {
+    fun welcome(
+        @RequestHeader("X-service-request") requestFrom: String
+    ) : String {
+        logger.info("RequestFrom : $requestFrom")
         return "welcome to second service!"
     }
 
