b1eaa37b58aacbd0db11452275afc3cefaa33718
Chulhee Lee <chulhee23@naver.com>
2022-03-06 17:53:22 +0900

    gateway LoadBalancer 확인

--- second-service/src/main/kotlin/im/chulhee/secondservice/controller/SecondServiceController.kt
+++ second-service/src/main/kotlin/im/chulhee/secondservice/controller/SecondServiceController.kt
@@ -1,16 +1,20 @@
 package im.chulhee.secondservice.controller
 
 import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/second-service")
-
-class SecondServiceController {
+class SecondServiceController(
+    private val env: Environment
+) {
     private val logger = LoggerFactory.getLogger(SecondServiceController::class.java)
+
 
     @GetMapping("/welcome")
     fun welcome(
@@ -21,8 +25,9 @@ class SecondServiceController {
     }
 
     @GetMapping("/check")
-    fun check(): String {
-        return "check second service"
+    fun check(request: HttpServletRequest): String {
+        logger.info("Server port = {}", request.serverPort)
+        return "check second service, PORT ${env.getProperty("local.server.port")}"
     }
 
 
